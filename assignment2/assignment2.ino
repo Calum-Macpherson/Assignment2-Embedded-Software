ab8ec101f7b44309504b1f2e3eca51235fbff4ec
Calum-Macpherson <99350334+Calum-Macpherson@users.noreply.github.com>
2022-03-11 15:08:34 +0000

    Add files via upload

--- assignment2/assignment2.ino
+++ assignment2/assignment2.ino
@@ -21,42 +21,38 @@ Ticker myTick; // Initialises the ticker
 
 void setup() {
   Serial.begin(9600);
+  // input and outputs  pins initiallised 
   pinMode(signalA_LED, OUTPUT); 
   pinMode(signalB_LED, OUTPUT);
   pinMode(signal_check, OUTPUT);
   pinMode(input_button, INPUT);
   pinMode(input_V,INPUT);
   pinMode(inputV_task3,INPUT);
-  myTick.attach_ms(4,myCycle);//Ticker set up to "tick"  every 4ms to accomodate for a worse case scenario
+  myTick.attach_ms(4,myCycle);//Every 4ms, myCycle is called
 }
 void loop(){
   
 }
 
-void myCycle(){ 
+void myCycle(){ //Calls each of the tasks
   counter++;//increments the counter each time the cycle is ran
   digitalWrite(signalB_LED, HIGH);
-  delayMicroseconds(50);
+  delayMicroseconds(50);// The signal is high for 50 micro seconds, the same as in assignment 1
   digitalWrite(signalB_LED,LOW);
   /*
   The counter values below have been calculated by dividing the
   original periods by 4, since the ticker "ticks" every 4ms.
-  */
-  
+
+  Each task is called once the counter reaches the required value.   
+  */  
   if (counter%50==0){ 
     task2();
   }
   if (counter%250==0){
-    //digitalWrite(signal_check,HIGH);
     task3();
-    //digitalWrite(signal_check,LOW);
   }
   if(counter%11==0){
-    //digitalWrite(signal_check,HIGH);
     task4();
-    //digitalWrite(signal_check,LOW);
-  }
-  if(counter%11==1){
     task5();
   }
   if (counter%25==0){
@@ -64,8 +60,6 @@ void myCycle(){
   }
   if (counter%75==0){
     task7();
-  }
-  if (counter%75==1){
     task8();
   }
   if (counter%1250==0){
@@ -75,17 +69,15 @@ void myCycle(){
 
 
 void task2(){
-  input_button_state= digitalRead(input_button);
+  input_button_state= digitalRead(input_button);//Reads in the state of the button. 
 }
 unsigned long task3(){
   duration=pulseIn(inputV_task3,HIGH);
-  freq=1/(duration*2*pow(10, -6));
-  //Serial.println(freq);
+  freq=1/(duration*2*pow(10, -6));//
 }
 float task4(){
   pot_val = analogRead(input_V);
   return pot_val;
-  //Serial.print(pot_val);
 }
 float task5(){
   total=0;
@@ -95,8 +87,7 @@ float task5(){
   }
   filtered_val=(total/4);  
   return total;
-  return filtered_val;
-  
+  return filtered_val;  
 }
 void task6(){
   for (int j=0;j<1000;j++){
@@ -114,18 +105,16 @@ boolean task7(){
 }
 void task8(){
   if (error_code==1){
-    digitalWrite(signalA_LED, HIGH);
+    digitalWrite(signalA_LED, HIGH);// The yellow LED on my circuit will turn on when the error code is high.
   }
   else{
     digitalWrite(signalA_LED, LOW);
   }
 }
-
 void task9(){
   Serial.print(input_button_state);
   Serial.print (",  ");
   Serial.print(freq);
   Serial.print (",  ");
-  Serial.println(filtered_val);
-  
+  Serial.println(filtered_val);  
 }
